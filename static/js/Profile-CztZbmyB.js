1cf4e771db7719ce75543c908cba0ee73ed24d78
vincentxu77 <379130711@qq.com>
2025-03-29 12:42:23 +0800

    feat: 放开admin的saf

---
+++ static/js/Profile-CztZbmyB.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./Profile.vue_vue_type_script_setup_true_lang-CRVYzyYF.js";import"./index-Dva5DfmA.js";export{m as default};
--- static/js/Profile-DtDcK-wl.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./Profile.vue_vue_type_script_setup_true_lang-CReZEg8b.js";import"./index-ChEthMy4.js";export{m as default};
